0cf1a5e6af509ccf7c9bb32b739333ec1c9c701c
Anastasiia <nastyak25555@gmail.com>
2024-01-13 13:42:29 +0100

    New button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+   @IBOutlet var HelloWorldLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        HelloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
+        HelloWorldLabel.textColor = .systemRed
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showButtonPressed(_ sender: UIButton) {
+        HelloWorldLabel.isHidden.toggle()
+        
+        if HelloWorldLabel.isHidden {
+            showTextButton.setTitle("Show Text", for: .normal)
+        } else {
+            showTextButton.setTitle("Hide Text", for: .normal)
 
+        }
+    }
 }
 
